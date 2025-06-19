f6b96746fed8aeb386f797b04d36f4684cc2a7a8
Bariya Vishwas <bariyavasu46@gmail.com>
2025-06-19 08:08:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,12 +9,42 @@ function convertToRoman(num) {
       6:['I', 1]
     };
 
-  //your code here
+    let result = '';
+  for (const { val, sym } of romanMap) {
+    while (num >= val) {
+      result += sym;
+      num -= val;
+    }
+  }
+  return result;
+}
+
+// Function to convert Roman numeral to number
+function fromRoman(str) {
+  const romanToInt = {
+    I: 1, V: 5, X: 10, L: 50,
+    C: 100, D: 500, M: 1000,
+  };
+
+  let result = 0;
+  for (let i = 0; i < str.length; i++) {
+    const current = romanToInt[str[i]];
+    const next = romanToInt[str[i + 1]];
+
+    if (next && next > current) {
+      result += next - current;
+      i++; // Skip the next character
+    } else {
+      result += current;
+    }
+  }
+  return result;
+}
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+console.log(convertToRoman(36));
 
 
 
